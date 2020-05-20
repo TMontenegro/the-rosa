4c4e5d2e8777e6e1acea00f8bcaf3db4fa33730a
Tales Dias <montenegro.tales@gmail.com>
2020-05-20 01:25:24 -0300

    add animations

--- index.js
+++ index.js
@@ -3,9 +3,40 @@ const selectElement = (element) => {
   return document.querySelector(element);
 };
 
-let menuToggler = selectElement('.menu-toggle');
-let bodyToggler = selectElement('body');
+let menuToggler = selectElement(".menu-toggle");
+let bodyToggler = selectElement("body");
 
-menuToggler.addEventListener('click', function () {
-  bodyToggler.classList.toggle('open');
+menuToggler.addEventListener("click", function () {
+  bodyToggler.classList.toggle("open");
+});
+
+// Scroll reveal
+window.sr = ScrollReveal();
+
+sr.reveal(".animate-left", {
+  origin: "left",
+  duration: 1000,
+  distance: "25rem",
+  delay: 300,
+});
+
+sr.reveal(".animate-right", {
+  origin: "right",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
+});
+
+sr.reveal(".animate-top", {
+  origin: "top",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
+});
+
+sr.reveal(".animate-bottom", {
+  origin: "bottom",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
 });
